52601dd4192965321cfda9a745d91d19643fee3b
zim <renegadez@tut.by>
2023-11-02 15:58:50 +0300

    Add repositoryImpl

---
+++ app/src/main/java/com/example/buyerslist/data/ShopListRepositoryImpl.kt
@@ -0,0 +1,39 @@
+package com.example.buyerslist.data
+
+import com.example.buyerslist.domain.ShopItem
+import com.example.buyerslist.domain.ShopListRepository
+import java.lang.RuntimeException
+
+class ShopListRepositoryImpl: ShopListRepository {
+
+    private val shopList = mutableListOf<ShopItem>()
+
+    private var autoIncrementId = 0
+
+    override fun addShopItem(shopItem: ShopItem) {
+        if (shopItem.id == ShopItem.UNDEFINED_ID) {
+            shopItem.id = autoIncrementId++
+        }
+        shopList.add(shopItem)
+    }
+
+    override fun deleteShopItem(shopItem: ShopItem) {
+        shopList.remove(shopItem)
+    }
+
+    override fun editShopItem(shopItem: ShopItem) {
+        val oldElement = getShopItem(shopItem.id)
+        shopList.remove(oldElement)
+        addShopItem(shopItem)
+    }
+
+    override fun getShopItem(shopItemId: Int): ShopItem {
+        return shopList.find {
+            it.id == shopItemId
+        } ?: throw RuntimeException("Element with id $shopItemId not found")
+    }
+
+    override fun getShopList(): List<ShopItem> {
+         return shopList.toList()
+    }
+}

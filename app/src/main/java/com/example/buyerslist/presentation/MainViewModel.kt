6c3fec902ae3be2db21bcb4816a198507f227b4e
zim <renegadez@tut.by>
2023-12-04 16:17:38 +0300

    ViewModelScope

--- app/src/main/java/com/example/buyerslist/presentation/MainViewModel.kt
+++ app/src/main/java/com/example/buyerslist/presentation/MainViewModel.kt
@@ -2,6 +2,7 @@ package com.example.buyerslist.presentation
 
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.buyerslist.data.ShopListRepositoryImpl
 import com.example.buyerslist.domain.DeleteShopItemUseCase
 import com.example.buyerslist.domain.EditShopItemUseCase
@@ -20,25 +21,18 @@ class MainViewModel(application: Application) : AndroidViewModel(application) {
     private val deleteShopItemUseCase = DeleteShopItemUseCase(repository)
     private val editShopItemUseCase = EditShopItemUseCase(repository)
 
-    private val scope = CoroutineScope(Dispatchers.IO)
-
     val shopList = getShopListUseCase.getShopList()
 
     fun deleteShopItem(shopItem: ShopItem) {
-        scope.launch {
+        viewModelScope.launch {
             deleteShopItemUseCase.deleteShopItem(shopItem)
         }
     }
 
     fun changeEnableState(shopItem: ShopItem) {
-        scope.launch {
+        viewModelScope.launch {
             val newItem = shopItem.copy(enabled = !shopItem.enabled)
             editShopItemUseCase.editShopItem(newItem)
         }
-    }
-
-    override fun onCleared() {
-        super.onCleared()
-        scope.cancel()
     }
 }
